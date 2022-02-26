66285f933d13338f74a8c72923945dc8627771d7
chenyulue <chenyulue@163.com>
2022-02-26 23:04:29 +0800

    c-example

---
+++ chapter-01/recipe-04/c-example/hello-world.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include "message.h"
+
+int main(int argc, char *argv[])
+{
+    say_hello("Hello, CMake World!");
+    say_hello("Goodbye, CMake World!");
+
+    return EXIT_SUCCESS;
+}
---
+++ chapter-01/recipe-05/c-example/message.h
@@ -0,0 +1,6 @@
+#ifndef MESSAGE_H_
+#define MESSAGE_H_
+
+void say_hello(const char * message);
+
+#endif
